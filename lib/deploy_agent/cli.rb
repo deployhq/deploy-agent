56f2e9aa184dfbc429a191242a443095b5c745f7
Adam Cooke <me@adamcooke.io>
2017-03-08 14:09:59 +0000

    tidy cli dispatcher

--- lib/deploy_agent/cli.rb
+++ lib/deploy_agent/cli.rb
@@ -1,23 +1,66 @@
 module DeployAgent
   class CLI
-    def run(arguments)
-      case arguments[0]
-      when 'status'
-        status
-      when 'start'
-        start_server
-      when 'stop'
-        stop_server
-      when 'restart'
-        restart_server
-      when 'run'
-        run_server
-      when 'setup'
-        CertificateManager.new.generate_certificate
+
+    def dispatch(arguments)
+      methods = self.public_methods(false).delete_if { |n| n == :dispatch }.sort
+      if arguments[0] && methods.include?(arguments[0].to_sym)
+        public_send(arguments[0])
       else
-        puts "Usage: deploy-agent [start|stop|restart|status|run|setup]"
+        puts "Usage: deploy-agent [#{methods.join('|')}]"
       end
     end
+
+    def setup
+      CertificateManager.new.generate_certificate
+    end
+
+    def restart
+      stop
+      while(is_running?)
+        sleep 0.5
+      end
+      start
+    end
+
+    def start
+      if is_running?
+        puts "Deploy agent already running. Process ID #{pid_from_file}"
+        Process.exit(1)
+      else
+        pid = fork do
+          $background = true
+          write_pid
+          run
+        end
+        puts "Deploy agent started. Process ID #{pid}"
+        Process.detach(pid)
+      end
+    end
+
+    def stop
+      if is_running?
+        pid = pid_from_file
+        Process.kill('TERM', pid)
+        puts "Deploy agent stopped. Process ID #{pid}"
+      else
+        puts "Deploy agent is not running"
+        Process.exit(1)
+      end
+    end
+
+    def status
+      if is_running?
+        puts "Deploy agent is running. PID #{pid_from_file}"
+      else
+        puts "Deploy agent is not running."
+      end
+    end
+
+    def run
+      Agent.new.run
+    end
+
+    private
 
     def is_running?
       if pid = pid_from_file
@@ -41,52 +84,6 @@ module DeployAgent
     def write_pid
       File.open(PID_PATH, 'w') { |f| f.write Process.pid.to_s }
       at_exit { File.delete(PID_PATH) if File.exists?(PID_PATH) }
-    end
-
-    def restart_server
-      stop_server
-      while(is_running?)
-        sleep 0.5
-      end
-      start_server
-    end
-
-    def start_server
-      if is_running?
-        puts "Deploy agent already running. Process ID #{pid_from_file}"
-        Process.exit(1)
-      else
-        pid = fork do
-          $background = true
-          write_pid
-          run_server
-        end
-        puts "Deploy agent started. Process ID #{pid}"
-        Process.detach(pid)
-      end
-    end
-
-    def stop_server
-      if is_running?
-        pid = pid_from_file
-        Process.kill('TERM', pid)
-        puts "Deploy agent stopped. Process ID #{pid}"
-      else
-        puts "Deploy agent is not running"
-        Process.exit(1)
-      end
-    end
-
-    def status
-      if is_running?
-        puts "Deploy agent is running. PID #{pid_from_file}"
-      else
-        puts "Deploy agent is not running."
-      end
-    end
-
-    def run_server
-      Agent.new.run
     end
 
   end
